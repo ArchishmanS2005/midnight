ed88c571b010c139d1b12ba4231ae10f31c5ef72
ArchishmanS2005 <archishmansarkar94@gmail.com>
2026-07-27 09:09:44 +0000

    feat(contract): add TypeScript compilation bindings and witness providers

---
+++ contract/src/index.ts
@@ -0,0 +1,14 @@
+import { CompiledContract } from "@midnight-ntwrk/midnight-js-protocol/compact-js";
+
+export * from "./managed/credshield/contract/index.js";
+export * from "./witnesses.js";
+
+import * as CompiledCredShieldContractModule from "./managed/credshield/contract/index.js";
+import * as Witnesses from "./witnesses.js";
+
+export const CompiledCredShieldContract = CompiledContract.make<
+  CompiledCredShieldContractModule.Contract<Witnesses.CredShieldPrivateState>
+>("CredShield", CompiledCredShieldContractModule.Contract<Witnesses.CredShieldPrivateState>).pipe(
+  CompiledContract.withWitnesses(Witnesses.witnesses),
+  CompiledContract.withCompiledFileAssets("./managed/credshield"),
+);
