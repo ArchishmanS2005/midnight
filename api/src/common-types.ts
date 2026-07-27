e79c6bc729f8dbd3111a0fbef7d9533aed6098c3
ArchishmanS2005 <archishmansarkar94@gmail.com>
2026-07-27 09:09:44 +0000

    feat(api): add common types and state mapping for CredShield

---
+++ api/src/common-types.ts
@@ -0,0 +1,29 @@
+import { type MidnightProviders } from '@midnight-ntwrk/midnight-js-types';
+import { type FoundContract } from '@midnight-ntwrk/midnight-js-contracts';
+import type { CredentialState, CredShieldPrivateState, Contract, Witnesses } from '../../contract/src/index';
+
+export const credShieldPrivateStateKey = 'credShieldPrivateState';
+export type PrivateStateId = typeof credShieldPrivateStateKey;
+
+export type PrivateStates = {
+  readonly credShieldPrivateState: CredShieldPrivateState;
+};
+
+export type CredShieldContract = Contract<CredShieldPrivateState, Witnesses<CredShieldPrivateState>>;
+
+export type CredShieldCircuitKeys = Exclude<keyof CredShieldContract['impureCircuits'], number | symbol>;
+
+export type CredShieldProviders = MidnightProviders<CredShieldCircuitKeys, PrivateStateId, CredShieldPrivateState>;
+
+export type DeployedCredShieldContract = FoundContract<CredShieldContract>;
+
+export type CredShieldDerivedState = {
+  readonly credentialState: CredentialState;
+  readonly credentialId: string;
+  readonly credentialMetadata: string | undefined;
+  readonly issuerAuthority: string;
+  readonly totalIssued: bigint;
+  readonly totalVerified: bigint;
+  readonly sequence: bigint;
+  readonly isIssuer: boolean;
+};
